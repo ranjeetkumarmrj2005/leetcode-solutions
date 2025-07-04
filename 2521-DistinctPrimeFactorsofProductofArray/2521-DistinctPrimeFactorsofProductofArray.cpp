5f1e4fbdbf704b9466d726029b3b809b9af8f70f
RANJEET VERMA <ranjeetkumarmrj2005@gmail.com>
2025-07-04 19:16:41 +0530

    File updated at 7/4/2025, 7:16:40 PM

--- 2521-DistinctPrimeFactorsofProductofArray/2521-DistinctPrimeFactorsofProductofArray.cpp
+++ 2521-DistinctPrimeFactorsofProductofArray/2521-DistinctPrimeFactorsofProductofArray.cpp
@@ -1,19 +1,32 @@
-// Last updated: 7/4/2025, 5:58:06 PM
+// Last updated: 7/4/2025, 7:16:40 PM
 class Solution {
 public:
-    int count=0;
-    bool isPrime(int n){
-        if(n<=1) return false;
-        for(int i=2;i<=sqrt(n);i++){
-            if(n%i==0) return false;
-        }
-        return true;
+bool isPrime(int n){
+    if(n==1) return false;
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0) return false;
     }
-    int minSteps(int n) {
-       if(isPrime(n)) return n;
-        for(int i=2;i<=sqrt(n);i++){
-            if(n%i==0) return i+ minSteps(n/i);
-       } 
-       return 0;
+    return true;
+}
+bool factor(int n){
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0 && isPrime(i)){
+            if(30%i!=0) return false;
+            // else false;
+        }
+    }
+    for(int i=sqrt(n);i>1;i--){
+        if(n%(n/i)==0 && isPrime(n/i)){
+            if(30%(n/i)!=0) return false;
+            // else false;
+        }
+    }
+    return true;
+}
+    bool isUgly(int n) {
+        if(n<=0) return false;
+        if(n==1||n==2||n==3||n==5) return true;
+        if(isPrime(n)) return false;
+        return factor(n);
     }
 };
