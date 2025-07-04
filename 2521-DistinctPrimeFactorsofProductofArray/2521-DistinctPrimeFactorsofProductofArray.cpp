abd05da888edc76d9a199729bc2e20cabc3bfa55
RANJEET VERMA <ranjeetkumarmrj2005@gmail.com>
2025-07-04 19:22:48 +0530

    File updated at 7/4/2025, 7:22:48 PM

--- 2521-DistinctPrimeFactorsofProductofArray/2521-DistinctPrimeFactorsofProductofArray.cpp
+++ 2521-DistinctPrimeFactorsofProductofArray/2521-DistinctPrimeFactorsofProductofArray.cpp
@@ -1,4 +1,4 @@
-// Last updated: 7/4/2025, 7:16:40 PM
+// Last updated: 7/4/2025, 7:22:48 PM
 class Solution {
 public:
 bool isPrime(int n){
@@ -12,21 +12,20 @@ bool factor(int n){
     for(int i=2;i<=sqrt(n);i++){
         if(n%i==0 && isPrime(i)){
             if(30%i!=0) return false;
-            // else false;
         }
     }
     for(int i=sqrt(n);i>1;i--){
         if(n%(n/i)==0 && isPrime(n/i)){
             if(30%(n/i)!=0) return false;
-            // else false;
         }
     }
     return true;
 }
     bool isUgly(int n) {
         if(n<=0) return false;
-        if(n==1||n==2||n==3||n==5) return true;
-        if(isPrime(n)) return false;
-        return factor(n);
+        if(isPrime(n)&& n>5) return false;
+        else if(isPrime(n)&& 30%n==0) return true;
+        else return factor(n);
+         
     }
 };
