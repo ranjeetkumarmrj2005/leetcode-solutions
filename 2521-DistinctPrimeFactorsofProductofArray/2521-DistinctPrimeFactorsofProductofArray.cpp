8d8f01848bf521bbfa99722e3b3697ba96f512e2
RANJEET VERMA <ranjeetkumarmrj2005@gmail.com>
2025-07-04 17:49:56 +0530

    File updated at 7/4/2025, 5:49:55 PM

--- 2521-DistinctPrimeFactorsofProductofArray/2521-DistinctPrimeFactorsofProductofArray.cpp
+++ 2521-DistinctPrimeFactorsofProductofArray/2521-DistinctPrimeFactorsofProductofArray.cpp
@@ -1,22 +1,19 @@
-// Last updated: 7/4/2025, 5:37:44 PM
+// Last updated: 7/4/2025, 5:49:55 PM
 class Solution {
 public:
-    int count=0;
-    bool isPrime(int n){
-        if(n<=1) return false;
-        for(int i=2;i<=sqrt(n);i++){
-            if(n%i==0) return false;
-        }
-        return true;
-    }
+    // int count=0;
+    // bool isPrime(int n){
+    //     if(n<=1) return false;
+    //     for(int i=2;i<=sqrt(n);i++){
+    //         if(n%i==0) return false;
+    //     }
+    //     return true;
+    // }
     int minSteps(int n) {
-        if(isPrime(n)) return count+n;
+       if(n==1) return 0;
         for(int i=2;i<=sqrt(n);i++){
-            if(n%i==0) {
-                count=count+i;
-                return minSteps(n/i);
-            }
+            if(n%i==0) return i+ minSteps(n/i);
        } 
-       return count;
+       return n;
     }
 };
