ec7616a0875f1c0fb90b295f3a276570a676aa56
RANJEET VERMA <ranjeetkumarmrj2005@gmail.com>
2025-07-14 13:47:40 +0530

    Create 1510-FindLuckyIntegerInAnArray

---
+++ 1510-FindLuckyIntegerInAnArray/1510-FindLuckyIntegerInAnArray.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/14/2025, 1:47:41 PM
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        if(n==1){
+            if(arr[0]==1) return 1;
+            else return -1;
+        }
+        int count=1;
+        int ans=-1;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]==arr[i+1]) count++;
+            else if(count==arr[i]){
+                ans=arr[i];
+                count =1;
+            }
+            else count=1;
+        }
+        if(count==arr[n-2]) ans = arr[n-2];
+        return ans;
+    }
+};
