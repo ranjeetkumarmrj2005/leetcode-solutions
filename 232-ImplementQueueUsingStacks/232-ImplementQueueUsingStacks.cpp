8e9bbf7b213abfd9223efbb169e5ba41c801250d
RANJEET VERMA <ranjeetkumarmrj2005@gmail.com>
2026-07-08 18:49:40 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.cpp
@@ -0,0 +1,36 @@
+// Last updated: 7/8/2026, 6:49:40 PM
+class MyQueue {
+public:
+    stack<int>st;
+    stack<int>helper;
+    MyQueue() {}
+    void push(int x) {
+        if(st.size()==0) {
+            st.push(x);
+            return;
+        } 
+        else{
+            while(st.size()>0){
+                helper.push(st.top());
+                st.pop();
+            }
+        }
+        helper.push(x);
+        while(helper.size()>0){
+            st.push(helper.top());
+            helper.pop();
+        }
+    }
+    int pop() {
+        int x=st.top();
+        st.pop();
+        return x;
+    }
+    int peek() {
+        return st.top();
+    }
+    bool empty() {
+        if(st.size()==0) return true;
+        else return false;
+    }
+};
